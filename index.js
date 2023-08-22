0e0ba542f8fd3d8b494f79bc578440fd384dadb5
Great Ochuko <greatochuko@gmail.com>
2023-08-22 08:24:45 +0100

    complete REST api

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 const whiteList = ["http://127.0.0.1:5500", "http://localhost:3000"];
 const corsOptions = {
   origin: (origin, callback) => {
-    console.log("ORIGIN: ", origin);
     if (whiteList.includes(origin) || !origin) {
       callback(null, true);
     } else {
@@ -40,14 +39,68 @@ app
   })
   .post((req, res) => {
     const task = req.body.task;
-    const task2 = req.body;
-    console.log("New Task: ", task2);
     const newTask = new Task({
       item: task,
       isDone: false,
     });
     newTask.save();
     res.json(newTask);
+  })
+  .delete(async (req, res) => {
+    try {
+      await Task.deleteMany({});
+      res.json("All Tasks Deleted Successfully");
+    } catch (err) {
+      res.status(404).json({ Error: err.message });
+    }
+  });
+
+app
+  .route("/task/:id")
+  .get(async (req, res) => {
+    try {
+      const targetTask = await Task.findById(id);
+      if (!targetTask) {
+        throw new Error("Task with id does not exist");
+      }
+      res.status(200).json(targetTask);
+    } catch (err) {
+      res.status(404).json({ Error: err.message });
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      await Task.findOneAndReplace({ _id: req.params.id }, req.body);
+      res.json("Task Replaced Successfully");
+    } catch (err) {
+      res.status(404).json({ Error: err.message });
+    }
+  })
+  .patch(async (req, res) => {
+    const id = req.params.id;
+    try {
+      const targetTask = await Task.findById(id);
+      if (!targetTask) {
+        throw new Error("Task with id does not exist");
+      }
+      await Task.findByIdAndUpdate(id, req.body);
+      res.json("Task Updated Successfully!");
+    } catch (err) {
+      res.status(404).json({ Error: err.message });
+    }
+  })
+  .delete(async (req, res) => {
+    const id = req.params.id;
+    try {
+      const targetTask = await Task.findById(id);
+      if (!targetTask) {
+        throw new Error("Task with id does not exist");
+      }
+      await Task.findByIdAndDelete(id);
+      res.status(200).json("Task Deleted Successfully");
+    } catch (err) {
+      res.status(404).json({ Error: err.message });
+    }
   });
 
 app.listen(PORT, () => {
